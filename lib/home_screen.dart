d528fb822b08fd766b197796538d620f00bf49ac
Abhaykevat <abhaykevat84@gmail.com>
2024-06-19 00:23:00 +0530

    bottomsheet

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -16,10 +16,76 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('getx Tutorials'),
       ),
       body: Column(children: [
+        Card(
+          child: ListTile(
+            title: Text('Dialog alert'),
+            
+            subtitle: Text("Getx dialog Alert with getx "),
+            onTap: (){
+              Get.defaultDialog(
+                title: "Delete Chat",
+                contentPadding: EdgeInsets.all(20),
+                titlePadding: EdgeInsets.only(top: 20),
+                textConfirm: "Yes",
+                // textCancel: "NO",
+                // middleText: "Are You Sure"
+                confirm: TextButton(onPressed: (){
+                  // Navigator.pop(context);
+                  Get.back();
+                }, child: Text('OK')),
+                cancel: TextButton(onPressed: (){}, child: Text('Cancel')),
+                content: Column(
+                  children: [
+                    Text('welcome'),
+                    Text('welcome back '),
+                    Text("Welocome Again"),
+                    Text('welcome'),
+                    Text('welcome back '),
+                    Text("Welocome Again")
+                  ],
+                )
+              );
+            },
+          ),
+        ),
+        Card(
+          child: ListTile(
+            title: Text('Getx Bottom Sheet'),
+            
+            subtitle: Text("Getx dialog Alert with getx "),
+            onTap: (){
+              Get.bottomSheet(
+                Container(
+                  // color: Colors.red,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.red
+                  ),
+                  child: Column(children: [
+                    ListTile(
+                      leading: Icon(Icons.light_mode),
+                      title: Text('Light Theme'),
+                      onTap: (){
+                        Get.changeTheme(ThemeData.light());
+                      },
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.light_mode),
+                      title: Text('Dark Theme'),
+                      onTap: (){
+                        Get.changeTheme(ThemeData.dark());
+                      },
+                    ),
+                  ]),
+                )
+              );
+            },
+          ),
+        ),
       ]),
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        Get.snackbar("Abhay Kevat", "Follow to Page",mainButton: TextButton(onPressed: (){}, child: Text("Button")),duration:Duration(seconds: 5) ,snackPosition: SnackPosition.BOTTOM,backgroundColor: Colors.white,colorText: Colors.blue,icon: Icon(Icons.facebook));
-      }),
+      // floatingActionButton: FloatingActionButton(onPressed: (){
+      //   Get.snackbar("Abhay Kevat", "Follow to Page",mainButton: TextButton(onPressed: (){}, child: Text("Button")),duration:Duration(seconds: 5) ,snackPosition: SnackPosition.BOTTOM,backgroundColor: Colors.white,colorText: Colors.blue,icon: Icon(Icons.facebook));
+      // }),
     );
   }
 }
